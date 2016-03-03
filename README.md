5016be7463bc2480b1de3158d7e7ad23efb5e0bf
Mike Bannister <notimpossiblemike@gmail.com>
2016-03-03 09:10:59 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,19 @@
+# Course playground
+
+Make it easy to bring up different builds of player
+
+## Adding a build
+
+`./build-player.js <commitish>`
+
+E.g.
+
+`./build-player.js master`
+
+## Viewing a build
+
+Build a URL specifying a commit and other options
+
+```
+https://github.com/Versal/course-playground/?commit=dbe907&apiHost=stagingstack.versal.com&sessionId=89a55ac4-0d07-4b77-b66f-80ae99ff1c87&courseId=bboy&initialRevision=DRAFT&initialMode=LEARNING
+```
---
+++ build-player.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const sh = require('shelljs')
+const tmp = require('tmp')
+const commitish = process.argv[2]
+
+if (!commitish) {
+  console.error('Usage: ./build-player.js <commitish>')
+  process.exit(1)
+}
+
+const REPO_URL = 'git@github.com:Versal/playa.git'
+
+const tempPath = tmp.dirSync().name
+const playaPath = `${tempPath}/playa`
+
+sh.exec(`git clone ${REPO_URL} ${playaPath}`)
+sh.cd(playaPath)
+
+sh.exec(`git checkout ${commitish}`)
+sh.exec('npm install')
+sh.exec('bower install')
+
+sh.exec(`npm run build`)
+const shortSha = sh.exec(`git rev-parse HEAD`).stdout.trim().slice(0, 6)
+
+try {
+  sh.exec('git checkout gh-pages')
+} catch(e) {
+  sh.exec('git checkout -b gh-pages')
+}
+
+sh.cd(__dirname)
+
+const commitsPath = `${__dirname}/commits`
+sh.mkdir('-p', commitsPath)
+
+sh.rm('-rf', `${commitsPath}/${shortSha}`)
+sh.cp('-r', `${playaPath}/dist`, `${commitsPath}/${shortSha}`)
+sh.rm('-rf', `${__dirname}/bower_components`)
+sh.cp('-r', `${playaPath}/bower_components`, `${__dirname}/bower_components`)
+
+sh.exec('git add ./commits')
+sh.exec(`git commit -m'Added player commit ${shortSha}'`)
